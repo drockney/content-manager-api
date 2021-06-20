51b8732de76080b1ff3b6ea1792af556e167353b
Doug Rockney <github@rockney.org>
2021-06-20 13:15:54 -0400

    Added PORT environment variable reference

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const pathToFile = path.resolve("./data.json");
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 
